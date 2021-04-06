049d83efdbcd17aaea50ed2e9ef6295cc7864b07
xinglong-zh <xinglong_zh@foxmail.com>
2021-04-06 18:06:04 +0800

    xx

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,34 @@
-module.exports ={
-    title:'note demo',
-    description:'a note space base on vue press',
-    base:'/note/', // github访问路径，
-    home:true,
-    themeConfig:{
+module.exports = {
+    title: 'note demo',
+    description: 'a note space base on vue press',
+    base: '/note/', // github访问路径，
+    home: true,
+    themeConfig: {
         // 导航栏配置
-        nav:[
-            {text:'home',link:'/'},
-            {text:'guide',link:'/guide'},
-            {text:'demo',link:'/www.baidu.com'}
+        nav: [
+            { text: 'home', link: '/' },
+            {
+                text: 'demo',
+                items: [
+                    { text: 'todo', link: '/todo/' },
+                    { text: 'server', link: '/server/' },
+                    { text: 'fed', link: '/fed/' },
+
+                ]
+            }
         ],
         // 侧边栏配置
-        sidebar:[
-            ['/','资料'],
-            ['/ts/','TS+JS'],
-            ['/vite/','VITE+VUE'],
-            ['/golang/','Golang'],
-            ['/java/','JAVA'],
-            ['/nginx/','Nginx'],
-            ['/k8s/','K8s'],
-            ['/todo/','todos'],
-        ],
-        smoothScroll:true,
+        sidebar: {
+            '/todo/': [
+                '',
+                'flex',
+                'sass'
+            ],
+            '/deploy/':[],
+            '/server/':[],
+            '/fed/':[],
+            '/': [''],
+        },
+        smoothScroll: true,
     }
 }
